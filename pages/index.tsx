3dc1fac39fe42e959c5e305c4064ad236a1c589a
adama-onuoha <progressonuoha20@gmail.com>
2025-09-21 14:41:24 -0700

    Added components and interfaces directories with empty files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-center mt-10">
-      Airbnb Application Clone system
-    </h1>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
+        Get Started
+      </button>
+    </main>
   );
 }
